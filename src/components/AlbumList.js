aa2e505e119cb4119e6323fae489c4a789537e82
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-30 18:36:29 +0200

    fetch data

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => (
-    <View>
-      <Text>Album List</Text>
-    </View>
-  );
+class AlbumList extends Component {
+  state = {
+    albums: []
+  };
+
+  componentDidMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        return new Error('Problem getting data from url');
+      })
+      .then(data => {
+        this.setState({ albums: data });
+      })
+      .catch(error => {
+        console.log(`There has been a problem with your fetch operation: ${error.message}`);
+      });
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Album List</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
