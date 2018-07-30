6ec7c9de432cf59470700adb61c5cf04445f92bf
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-30 18:24:14 +0200

    clear up

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header';
 
 export default class App extends React.Component {
