231f1a8184fccb81741fd6e6139531bb9e1112e3
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-30 18:21:55 +0200

    add header styling

--- App.js
+++ App.js
@@ -4,19 +4,6 @@ import Header from './src/components/Header';
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Header />
-      </View>
-    );
+    return <Header />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
