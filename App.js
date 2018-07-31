66155f0c279c8102124f01a6c83749dfe3cfb733
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-31 20:44:39 +0200

    add redux

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { View } from 'react-native';
-import Header from './src/components/Header';
-import AlbumList from './src/components/AlbumList';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <View>
-        <Header headerText={'Albums'} />
-        <AlbumList />
-      </View>
+      <Provider>
+        <View>
+          <Text>Hello</Text>
+        </View>
+      </Provider>
     );
   }
 }
