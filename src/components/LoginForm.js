bbc2ceac33b3a2151c18284aa068bca8c2a8ce19
Adam Marczyk <adam.marczyk90@gmail.com>
2018-08-01 19:42:10 +0200

    add change password action

---
+++ src/components/LoginForm.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { emailChanged, passwordChanged } from '../actions';
+import { Card, CardSection, Input, Button } from './common';
+
+class LoginForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
+
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
+            value={this.props.email}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="password"
+            onChangeText={this.onPasswordChange.bind(this)}
+            value={this.props.password}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>Text</Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+};
+
+const mapStateToProps = ({ auth }) => {
+  const { email, password } = auth;
+
+  return { email, password };
+};
+
+export default connect(mapStateToProps, {
+  emailChanged, passwordChanged
+})(LoginForm);
