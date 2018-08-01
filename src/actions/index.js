9191dfef788b14d3bfc610e9c5f18e9f407b9d7a
Adam Marczyk <adam.marczyk90@gmail.com>
2018-08-01 19:57:17 +0200

    add loginuser to reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
 
 export const emailChanged = text => ({
   type: EMAIL_CHANGED,
@@ -16,6 +16,6 @@ export const loginUser = ({ email, password }) => dispatch => {
     .auth()
     .signInWithEmailAndPassword(email, password)
     .then(user => {
-      dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
+      dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
     });
 };
