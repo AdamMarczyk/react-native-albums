a95aef8cbf74044644e55d02742dd82c0705c23e
Adam Marczyk <adam.marczyk90@gmail.com>
2018-08-01 19:46:40 +0200

    add common components

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './src/reducers';
+import LoginForm from './src/components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBR4FTGiOcTZ5OBNi6thE0KhCDR3PaOtQM',
+      authDomain: 'manager-2328b.firebaseapp.com',
+      databaseURL: 'https://manager-2328b.firebaseio.com',
+      storageBucket: 'manager-2328b.appspot.com',
+      messagingSenderId: '193075244299'
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
+    const store = createStore(reducers);
+
     return (
-      <Provider>
-        <View>
-          <Text>Hello</Text>
-        </View>
+      <Provider store={store}>
+        <LoginForm />
       </Provider>
     );
   }
