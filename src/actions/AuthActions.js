efd1a2dce714199a68148fbb60c198c0dd2e8abd
Adam Marczyk <adam.marczyk90@gmail.com>
2018-08-02 17:37:18 +0200

    add auth actions

--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -9,32 +9,32 @@ import {
 } from './types';
 
 export const emailChanged = text => ({
-    type: EMAIL_CHANGED,
-    payload: text
-  });
+  type: EMAIL_CHANGED,
+  payload: text
+});
 
 export const passwordChanged = text => ({
-    type: PASSWORD_CHANGED,
-    payload: text
-  });
+  type: PASSWORD_CHANGED,
+  payload: text
+});
 
 export const loginUser = ({ email, password }) => dispatch => {
-    dispatch({ type: LOGIN_USER });
+  dispatch({ type: LOGIN_USER });
 
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(user => loginUserSuccess(dispatch, user))
-      .catch(error => {
-        console.log(error);
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(user => loginUserSuccess(dispatch, user))
+    .catch(error => {
+      console.log(error);
 
-        firebase
-          .auth()
-          .createUserWithEmailAndPassword(email, password)
-          .then(user => loginUserSuccess(dispatch, user))
-          .catch(() => loginUserFail(dispatch));
-      });
-  };
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(user => loginUserSuccess(dispatch, user))
+        .catch(() => loginUserFail(dispatch));
+    });
+};
 
 const loginUserFail = dispatch => {
   dispatch({ type: LOGIN_USER_FAIL });
