352db8eb5d2c0af6f50c80d2a3c474b53aefb1c0
Adam Marczyk <adam.marczyk90@gmail.com>
2018-08-02 17:38:11 +0200

    format

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -6,7 +6,13 @@ import {
   LOGIN_USER
 } from '../actions/types';
 
-const INITIAL_STATE = { email: '', password: '', user: null, loading: false };
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  user: null,
+  error: '',
+  loading: false
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
@@ -19,7 +25,12 @@ export default (state = INITIAL_STATE, action) => {
     case LOGIN_USER_SUCCESS:
       return { ...state, ...INITIAL_STATE, user: action.payload };
     case LOGIN_USER_FAIL:
-      return { ...state, error: 'Authentication Failed', loading: false };
+      return {
+        ...state,
+        error: 'Authentication Failed.',
+        password: '',
+        loading: false
+      };
     default:
       return state;
   }
